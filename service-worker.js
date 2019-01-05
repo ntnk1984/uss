c65efd1393d9e652fff8ae27e89cf117d894959f
ntnk1984 <ntnk1984@gmail.com>
2019-01-05 18:11:07 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -32,6 +32,8 @@ self.addEventListener('install', function(e) {
   );
 });
 
+
+
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
@@ -57,3 +59,26 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open(dataCacheName).then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/uss/images/jeki_chan.jpg');
+      });
+    }
+  }));
+});
