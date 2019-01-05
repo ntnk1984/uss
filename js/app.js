191a9f04e4efbd49bd1e58ce0a57ee8024802d48
ntnk1984 <ntnk1984@gmail.com>
2019-01-05 18:23:51 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@
 // *** I set the header in order to solve the error above:
 // *** The value is set to "/" because this js is included in html file in upper folder.
 // *** I tried even "../" and many more others values...
-headers.append('Service-Worker-Allowed', '/');
+headers.append('Service-Worker-Allowed', '/uss/');
   
 console.log(headers.get('Service-Worker-Allowed'));
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('service-worker.js', {scope: '/uss/'}) 
+             .register('/uss/service-worker.js', {scope: '/uss/'}) 
              .then(function() {     console.log('Registration succeeded. Scope is ' + reg.scope);
             })
             .catch(function(err) {
